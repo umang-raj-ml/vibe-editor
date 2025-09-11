6605bd5f9763e6a1b8fdda96713dd943a6d02489
Umang Raj <Umang.Raj@xoriant.com>
2025-09-11 12:55:20 +0530

    landing page

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <Button>Get Started </Button>
-    </div>
-  );
-}
